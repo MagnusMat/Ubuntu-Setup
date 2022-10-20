20b9d868723041dabdd2f6071f6451710ec6d4e1
Magnus Mathiesen <magnus_mat@pm.me>
2022-10-20 14:16:17 +0200

    Automates extension installs

--- Ubuntu-Setup.sh
+++ Ubuntu-Setup.sh
@@ -51,12 +51,26 @@ flatpak install -y flathub com.mojang.Minecraft
 # Install Gnome Extensions
 sudo apt-get -y install gnome-tweaks
 sudo apt-get -y install gnome-shell-extensions
-xdg-open https://extensions.gnome.org/extension/545/hide-top-bar/
-xdg-open https://extensions.gnome.org/extension/1238/time/
-xdg-open https://extensions.gnome.org/extension/779/clipboard-indicator/
-xdg-open https://extensions.gnome.org/extension/1319/gsconnect/
-xdg-open https://github.com/tom-james-watson/Emote/wiki/Hotkey-In-Wayland
 flatpak install -y flathub com.mattjakeman.ExtensionManager
+
+array=( https://extensions.gnome.org/extension/545/hide-top-bar/
+https://extensions.gnome.org/extension/1238/time/
+https://extensions.gnome.org/extension/779/clipboard-indicator/
+https://extensions.gnome.org/extension/1319/gsconnect/
+https://github.com/tom-james-watson/Emote/wiki/Hotkey-In-Wayland/ )
+
+for i in "${array[@]}"
+do
+    EXTENSION_ID=$(curl -s $i | grep -oP 'data-uuid="\K[^"]+')
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=$EXTENSION_ID" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+    wget -O ${EXTENSION_ID}.zip "https://extensions.gnome.org/download-extension/${EXTENSION_ID}.shell-extension.zip?version_tag=$VERSION_TAG"
+    gnome-extensions install --force ${EXTENSION_ID}.zip
+    if ! gnome-extensions list | grep --quiet ${EXTENSION_ID}; then
+        busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${EXTENSION_ID}
+    fi
+    gnome-extensions enable ${EXTENSION_ID}
+    rm ${EXTENSION_ID}.zip
+done
 
 # Install R
 sudo apt-get -y install r-base r-base-dev
