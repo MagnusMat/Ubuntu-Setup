b9ac1c1089a7a7bfe4f40d922830a2a105e6b0f7
Magnus Mathiesen <magnus_mat@pm.me>
2022-12-01 09:41:27 +0100

    Added 1Password CLI and DotNet7

--- Ubuntu-Setup.sh
+++ Ubuntu-Setup.sh
@@ -83,6 +83,34 @@ install.packages("languageserver")
 install.packages("httpgd")
 q()
 
+# Install 1Password CLI
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+ sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+ sudo tee /etc/apt/sources.list.d/1password.list
+
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+ sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+ sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+sudo apt update && sudo apt install 1password-cli
+
+# Install DotNet
+sudo apt-get update && \
+  sudo apt-get install -y dotnet7
+
+sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-7.0
+
+sudo apt-get install -y dotnet-runtime-7.0
+
 # Install ProtonVPN
 wget `lynx -dump -listonly -nonumbers https://protonvpn.com/support/linux-ubuntu-vpn-setup/ | grep -E "*.deb" | head -1`
 sudo dpkg -i `find . -name 'protonvpn-stable-release_*'`
