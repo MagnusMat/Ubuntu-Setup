b7fef8bb13058e7ce8cf7a08e61242594a13a61e
Magnus Mathiesen <magnus_mat@pm.me>
2023-10-14 01:36:58 +0200

    Update Ubuntu-Setup.sh

--- Ubuntu-Setup.sh
+++ Ubuntu-Setup.sh
@@ -290,12 +290,6 @@ sudo apt install -y ./docker-desktop.deb
 
 yes | rm docker-desktop.deb
 
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
 # Draw.io
 sudo snap install drawio
 
